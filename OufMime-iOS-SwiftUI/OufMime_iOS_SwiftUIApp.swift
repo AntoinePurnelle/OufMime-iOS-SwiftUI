065f0d67ded05f2db6adebb92600de0af6d7513f
Antoine Purnelle <antoine.purnelle@gmail.com>
2022-03-17 14:04:20 +0100

    Initial Commit

---
+++ OufMime-iOS-SwiftUI/OufMime_iOS_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OufMime_iOS_SwiftUIApp.swift
+//  OufMime-iOS-SwiftUI
+//
+//  Created by Antoine Purnelle on 17/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct OufMime_iOS_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
