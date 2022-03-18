f748fe0a618e658d3b2c032e96c6e122e240a465
Antoine Purnelle <antoine.purnelle@gmail.com>
2022-03-18 13:40:12 +0100

    #2 View Model Implementation with tests

--- OufMime-iOS-SwiftUI/Model/WordViewModel.swift
+++ OufMime-iOS-SwiftUI/Model/WordViewModel.swift
@@ -7,9 +7,42 @@
 
 import Foundation
 
+typealias PlayedWord = (word: WordModel, found: Bool)
+
 class WordsViewModel: ObservableObject {
   private var repo: WordRepository!
-  @Published var words = [WordModel]()
+  
+  // Game Settings
+  public private(set) var timerTotalTime: Double = 40
+  public var timerCurrentTime: Double = 40
+  public private(set) var wordsCount = 40
+  public var categories = Dictionary(uniqueKeysWithValues: Category.allCases.map { ($0.rawValue, true) })
+  private var selectedCategories: [String] {
+    get {
+      categories.filter { (_, isSelected) in
+        isSelected
+      }.map { (category, _) in
+        category
+      }
+    }
+  }
+  
+  // Game Variables
+  public private(set) var currentTeam = -1
+  public private(set) var currentRound = 0
+  private var currentRoundFinished = true
+  @Published public private(set) var words = [WordModel] ()
+  private var teamWords: [[[WordModel]]] = [
+    [[WordModel](), [WordModel](), [WordModel]()],
+    [[WordModel](), [WordModel](), [WordModel]()],
+  ]
+  
+  public private(set) var wordsToPlay = [WordModel]()
+  private var wordsMissedInRound = [WordModel]()
+  public private(set) var wordsPlayedInTurn: [PlayedWord] = []
+  
+  public private(set) var currentWord: WordModel? = nil
+  
   
   init() {
     repo = WordRepositoryImpl()
@@ -27,7 +60,119 @@ class WordsViewModel: ObservableObject {
   }
 }
 
+// Getters/Setters Extension
+extension WordsViewModel {
+  
+  var hasMoreWords: Bool {
+    get { wordsToPlay.count > 0 }
+  }
+  
+  func getScore(inRound round: Int, forTeam team: Int) -> Int {
+    return currentRound < round ? -1 : teamWords[team][round].count
+  }
+  
+  func getCurrentRoundScore(forTeam team: Int) -> Int {
+    return getScore(inRound: currentRound, forTeam: team)
+  }
+  
+  func getTotalScore(forTeam team: Int) -> Int {
+    return teamWords[team].reduce(0) { count, words in
+      count + words.count
+    }
+  }
+  
+  var wordsFoundInTurnCount: Int {
+    get { wordsPlayedInTurn.filter { (_, found) in found }.count }
+  }
+  
+  var wordsMissedInTurnCount: Int {
+    get { wordsPlayedInTurn.filter { (_, found) in !found }.count }
+  }
+  
+  func editGamesSettings(withWordsCount wordsCount: Int, turnDuration seconds: Double) {
+    self.wordsCount = wordsCount
+    self.timerTotalTime = seconds
+  }
+  
+  func changeValueInPlayedWords(atRow index: Int) {
+    wordsPlayedInTurn[index].found.toggle()
+  }
+  
+}
 
+// Game LifeCycle Extensions
+extension WordsViewModel {
+
+  func initGame(onCompleted: @escaping (() -> Void)) {
+    repo.fetchRandomWords(inCategories: selectedCategories, withCount: wordsCount) { words in
+      self.words = words
+      print("Selected Words: \(words)")
+      
+      currentRound = 0
+      currentTeam = 0
+      
+      teamWords = [
+        [[WordModel](), [WordModel](), [WordModel]()],
+        [[WordModel](), [WordModel](), [WordModel]()],
+      ]
+      
+      DispatchQueue.main.async {
+        onCompleted()
+      }
+      
+    } onError: { message in
+      debugPrint(message)
+    }
+  }
+  
+  func initRound() {
+    currentRoundFinished = false
+    wordsToPlay.removeAll()
+    wordsToPlay.append(contentsOf: words.shuffled())
+    wordsMissedInRound.removeAll()
+    
+    print("Starting round \(currentRound) - Words to Play (\(wordsToPlay.count)): \(wordsToPlay)")
+  }
+  
+  func initTurn() {
+    wordsPlayedInTurn.removeAll()
+    currentWord = wordsToPlay.first
+    timerCurrentTime = timerTotalTime
+  }
+  
+  func playWord(wasFound: Bool, timerEnded: Bool) {
+    if hasMoreWords {
+      wordsPlayedInTurn.append((wordsToPlay.removeFirst(), wasFound))
+      
+      debugPrint("Played word \(wordsPlayedInTurn.last!)")
+      
+      if !timerEnded {
+        currentWord = wordsToPlay.first
+      }
+    }
+  }
+  
+  func finishTurn() {
+    let wordsFoundInTurn = wordsPlayedInTurn.filter { (word, found) in found }.map { (word, _) in word }
+    teamWords[currentTeam][currentRound].append(contentsOf: wordsFoundInTurn)
+    
+    let wordsMissedInTurn = wordsPlayedInTurn.filter { (word, found) in !found }.map { (word, _) in word }
+    wordsToPlay.append(contentsOf: wordsMissedInTurn)
+    
+    currentTeam = currentTeam == 0 ? 1 : 0
+    
+    debugPrint("Turn finished and saved")
+  }
+  
+  func finishRound() {
+    currentRound += 1
+    initRound()
+  }
+  
+}
+
+
+// DB Extensions
 extension WordsViewModel {
   
   private var wordsListVersion: Int {
